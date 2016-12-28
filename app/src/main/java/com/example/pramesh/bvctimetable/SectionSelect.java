89dd52afdd2e39b369e6663282cd5d6002033489
Suzal3579 <pe.messh@gmail.com>
2016-12-28 18:57:53 +0530

    Structure finished .... Firebase Implementation to all pages left to be done ....

--- app/src/main/java/com/example/pramesh/bvctimetable/SectionSelect.java
+++ app/src/main/java/com/example/pramesh/bvctimetable/SectionSelect.java
@@ -89,6 +89,7 @@ public class SectionSelect extends AppCompatActivity {
                     case "mechthirdsecond":
                         nextIntent = new Intent(SectionSelect.this, MechTable.class);
                         startActivity(nextIntent);
+                        break;
                     case "mechfourthfirst":
                         nextIntent = new Intent(SectionSelect.this, MechFourthFirst.class);
                         startActivity(nextIntent);
@@ -206,53 +207,176 @@ public class SectionSelect extends AppCompatActivity {
         section_b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(SectionSelect.this, "On Hold For Now! Please Stay Tuned For More Time Table Updates", Toast.LENGTH_LONG).show();
+                Intent nextIntent;
                 switch (value) {
                     case "csefirstfirst":
+                        nextIntent = new Intent(SectionSelect.this, CseFirstFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "csefirstsecond":
+                        nextIntent = new Intent(SectionSelect.this, CseFirstSecond.class);
+                        startActivity(nextIntent);
+                        break;
                     case "csesecondfirst":
+                        nextIntent = new Intent(SectionSelect.this, CseSecondFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "csesecondsecond":
+                        nextIntent = new Intent(SectionSelect.this, CseSecondSecond.class);
+                        startActivity(nextIntent);
+                        break;
                     case "csethirdfirst":
+                        nextIntent = new Intent(SectionSelect.this, CseThirdFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "csethirdsecond":
-
+                        nextIntent = new Intent(SectionSelect.this, CseTable.class);
+                        startActivity(nextIntent);
+                        break;
                     case "csefourthfirst":
+                        nextIntent = new Intent(SectionSelect.this, CseFourthFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "csefourthsecond":
+                        nextIntent = new Intent(SectionSelect.this, CseForthSecond.class);
+                        startActivity(nextIntent);
+                        break;
+
 
                     case "mechfirstfirst":
+                        nextIntent = new Intent(SectionSelect.this, MechFirstFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "mechfirstsecond":
+                        nextIntent = new Intent(SectionSelect.this, MechFirstSecond.class);
+                        startActivity(nextIntent);
+                        break;
                     case "mechsecondfirst":
+                        nextIntent = new Intent(SectionSelect.this, MechSecondFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "mechsecondsecond":
+                        nextIntent = new Intent(SectionSelect.this, MechSecondSecond.class);
+                        startActivity(nextIntent);
+                        break;
                     case "mechthirdfirst":
+                        nextIntent = new Intent(SectionSelect.this, MechThirdFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "mechthirdsecond":
+                        nextIntent = new Intent(SectionSelect.this, MechTable.class);
+                        startActivity(nextIntent);
+                        break;
                     case "mechfourthfirst":
+                        nextIntent = new Intent(SectionSelect.this, MechFourthFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "mechfourthsecond":
+                        nextIntent = new Intent(SectionSelect.this, MechFourthSecond.class);
+                        startActivity(nextIntent);
+                        break;
+
 
                     case "civilfirstfirst":
+                        nextIntent = new Intent(SectionSelect.this, CivilFirstFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "civilfirstsecond":
+                        nextIntent = new Intent(SectionSelect.this, CivilFirstSecond.class);
+                        startActivity(nextIntent);
+                        break;
                     case "civilsecondfirst":
+                        nextIntent = new Intent(SectionSelect.this, CivilSecondFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "civilsecondsecond":
+                        nextIntent = new Intent(SectionSelect.this, CivilSecondSecond.class);
+                        startActivity(nextIntent);
+                        break;
                     case "civilthirdfirst":
+                        nextIntent = new Intent(SectionSelect.this, CivilThirdFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "civilthirdsecond":
+                        nextIntent = new Intent(SectionSelect.this, CivilTable.class);
+                        startActivity(nextIntent);
+                        break;
                     case "civilfourthfirst":
+                        nextIntent = new Intent(SectionSelect.this, CivilFourthFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "civilfourthsecond":
+                        nextIntent = new Intent(SectionSelect.this, CivilFourthSecond.class);
+                        startActivity(nextIntent);
+                        break;
+
 
                     case "eeefirstfirst":
+                        nextIntent = new Intent(SectionSelect.this, EeeFirstFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "eeefirstsecond":
+                        nextIntent = new Intent(SectionSelect.this, EeeFirstSecond.class);
+                        startActivity(nextIntent);
+                        break;
                     case "eeesecondfirst":
+                        nextIntent = new Intent(SectionSelect.this, EeeSecondFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "eeesecondsecond":
+                        nextIntent = new Intent(SectionSelect.this, EeeSecondSecond.class);
+                        startActivity(nextIntent);
+                        break;
                     case "eeethirdfirst":
+                        nextIntent = new Intent(SectionSelect.this, EeeThirdFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "eeethirdsecond":
+                        nextIntent = new Intent(SectionSelect.this, EeeTable.class);
+                        startActivity(nextIntent);
+                        break;
                     case "eeefourthfirst":
+                        nextIntent = new Intent(SectionSelect.this, EeeFourthFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "eeefourthsecond":
+                        nextIntent = new Intent(SectionSelect.this, EeeFourthSecond.class);
+                        startActivity(nextIntent);
+                        break;
 
                     case "ecefirstfirst":
+                        nextIntent = new Intent(SectionSelect.this, EceFirstFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "ecefirstsecond":
+                        nextIntent = new Intent(SectionSelect.this, EceFirstSecond.class);
+                        startActivity(nextIntent);
+                        break;
                     case "ecesecondfirst":
+                        nextIntent = new Intent(SectionSelect.this, EceSecondFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "ecesecondsecond":
+                        nextIntent = new Intent(SectionSelect.this, EceSecondSecond.class);
+                        startActivity(nextIntent);
+                        break;
                     case "ecethirdfirst":
+                        nextIntent = new Intent(SectionSelect.this, EceThirdFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "ecethirdsecond":
+                        nextIntent = new Intent(SectionSelect.this, EceTable.class);
+                        startActivity(nextIntent);
+                        break;
                     case "ecefourthfirst":
+                        nextIntent = new Intent(SectionSelect.this, EceFourthFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "ecefourthsecond":
+                        nextIntent = new Intent(SectionSelect.this, EceFourthSecond.class);
+                        startActivity(nextIntent);
+                        break;
+
                 }
             }
         });
@@ -260,53 +384,176 @@ public class SectionSelect extends AppCompatActivity {
         section_c.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(SectionSelect.this, "On Hold For Now! Please Stay Tuned For More Time Table Updates", Toast.LENGTH_LONG).show();
-
+                Intent nextIntent;
                 switch (value) {
                     case "csefirstfirst":
+                        nextIntent = new Intent(SectionSelect.this, CseFirstFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "csefirstsecond":
+                        nextIntent = new Intent(SectionSelect.this, CseFirstSecond.class);
+                        startActivity(nextIntent);
+                        break;
                     case "csesecondfirst":
+                        nextIntent = new Intent(SectionSelect.this, CseSecondFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "csesecondsecond":
+                        nextIntent = new Intent(SectionSelect.this, CseSecondSecond.class);
+                        startActivity(nextIntent);
+                        break;
                     case "csethirdfirst":
+                        nextIntent = new Intent(SectionSelect.this, CseThirdFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "csethirdsecond":
+                        nextIntent = new Intent(SectionSelect.this, CseTable.class);
+                        startActivity(nextIntent);
+                        break;
                     case "csefourthfirst":
+                        nextIntent = new Intent(SectionSelect.this, CseFourthFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "csefourthsecond":
+                        nextIntent = new Intent(SectionSelect.this, CseForthSecond.class);
+                        startActivity(nextIntent);
+                        break;
+
 
                     case "mechfirstfirst":
+                        nextIntent = new Intent(SectionSelect.this, MechFirstFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "mechfirstsecond":
+                        nextIntent = new Intent(SectionSelect.this, MechFirstSecond.class);
+                        startActivity(nextIntent);
+                        break;
                     case "mechsecondfirst":
+                        nextIntent = new Intent(SectionSelect.this, MechSecondFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "mechsecondsecond":
+                        nextIntent = new Intent(SectionSelect.this, MechSecondSecond.class);
+                        startActivity(nextIntent);
+                        break;
                     case "mechthirdfirst":
+                        nextIntent = new Intent(SectionSelect.this, MechThirdFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "mechthirdsecond":
+                        nextIntent = new Intent(SectionSelect.this, MechTable.class);
+                        startActivity(nextIntent);
+                        break;
                     case "mechfourthfirst":
+                        nextIntent = new Intent(SectionSelect.this, MechFourthFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "mechfourthsecond":
+                        nextIntent = new Intent(SectionSelect.this, MechFourthSecond.class);
+                        startActivity(nextIntent);
+                        break;
+
 
                     case "civilfirstfirst":
+                        nextIntent = new Intent(SectionSelect.this, CivilFirstFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "civilfirstsecond":
+                        nextIntent = new Intent(SectionSelect.this, CivilFirstSecond.class);
+                        startActivity(nextIntent);
+                        break;
                     case "civilsecondfirst":
+                        nextIntent = new Intent(SectionSelect.this, CivilSecondFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "civilsecondsecond":
+                        nextIntent = new Intent(SectionSelect.this, CivilSecondSecond.class);
+                        startActivity(nextIntent);
+                        break;
                     case "civilthirdfirst":
+                        nextIntent = new Intent(SectionSelect.this, CivilThirdFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "civilthirdsecond":
+                        nextIntent = new Intent(SectionSelect.this, CivilTable.class);
+                        startActivity(nextIntent);
+                        break;
                     case "civilfourthfirst":
+                        nextIntent = new Intent(SectionSelect.this, CivilFourthFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "civilfourthsecond":
+                        nextIntent = new Intent(SectionSelect.this, CivilFourthSecond.class);
+                        startActivity(nextIntent);
+                        break;
+
 
                     case "eeefirstfirst":
+                        nextIntent = new Intent(SectionSelect.this, EeeFirstFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "eeefirstsecond":
+                        nextIntent = new Intent(SectionSelect.this, EeeFirstSecond.class);
+                        startActivity(nextIntent);
+                        break;
                     case "eeesecondfirst":
+                        nextIntent = new Intent(SectionSelect.this, EeeSecondFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "eeesecondsecond":
+                        nextIntent = new Intent(SectionSelect.this, EeeSecondSecond.class);
+                        startActivity(nextIntent);
+                        break;
                     case "eeethirdfirst":
+                        nextIntent = new Intent(SectionSelect.this, EeeThirdFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "eeethirdsecond":
+                        nextIntent = new Intent(SectionSelect.this, EeeTable.class);
+                        startActivity(nextIntent);
+                        break;
                     case "eeefourthfirst":
+                        nextIntent = new Intent(SectionSelect.this, EeeFourthFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "eeefourthsecond":
+                        nextIntent = new Intent(SectionSelect.this, EeeFourthSecond.class);
+                        startActivity(nextIntent);
+                        break;
 
                     case "ecefirstfirst":
+                        nextIntent = new Intent(SectionSelect.this, EceFirstFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "ecefirstsecond":
+                        nextIntent = new Intent(SectionSelect.this, EceFirstSecond.class);
+                        startActivity(nextIntent);
+                        break;
                     case "ecesecondfirst":
+                        nextIntent = new Intent(SectionSelect.this, EceSecondFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "ecesecondsecond":
+                        nextIntent = new Intent(SectionSelect.this, EceSecondSecond.class);
+                        startActivity(nextIntent);
+                        break;
                     case "ecethirdfirst":
+                        nextIntent = new Intent(SectionSelect.this, EceThirdFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "ecethirdsecond":
+                        nextIntent = new Intent(SectionSelect.this, EceTable.class);
+                        startActivity(nextIntent);
+                        break;
                     case "ecefourthfirst":
+                        nextIntent = new Intent(SectionSelect.this, EceFourthFirst.class);
+                        startActivity(nextIntent);
+                        break;
                     case "ecefourthsecond":
+                        nextIntent = new Intent(SectionSelect.this, EceFourthSecond.class);
+                        startActivity(nextIntent);
+                        break;
+
                 }
             }
         });
